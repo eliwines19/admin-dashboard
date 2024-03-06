4832c82cfd30a6323e50ad8983dbedbf05f1dcf6
Elijah Wines <wines.elijah@gmail.com>
2024-03-05 19:10:53 -0500

    create ThemeSettings Sidebar

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import './App.css'
 import { useStateContext } from './contexts/ContextProvider'
 
 const App = () => {
-    const { activeMenu } = useStateContext()
+    const { activeMenu, themeSettings, setThemeSettings } = useStateContext()
 
     return (
         <div>
@@ -19,9 +19,11 @@ const App = () => {
                     <div className="fixed right-4 bottom-4" style={{ zIndex: '1000' }}>
                         <TooltipComponent content="Settings" position="Top">
                             <button
-                            type="button"
-                            className="text-3xl padding-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                            style={{ background: 'blue', borderRadius: '50%' }}>
+                                type="button"
+                                className="text-3xl padding-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
+                                style={{ background: 'blue', borderRadius: '50%' }}
+                                onClick={() => setThemeSettings(true)}
+                            >
                                 <FiSettings />
                             </button>
                         </TooltipComponent>
@@ -41,6 +43,8 @@ const App = () => {
                         </div>
 
                         <div>
+                            {themeSettings && <ThemeSettings />}
+
                             <Routes>
                                 {/* Dashboard */}
                                 <Route path="/"element={<Ecommerce />}/>
