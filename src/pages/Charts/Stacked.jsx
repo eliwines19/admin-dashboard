b1ab77fba3279257309e1b6421b04497ffaa4b96
Elijah Wines <wines.elijah@gmail.com>
2024-03-10 12:20:12 -0400

    completed Pyramid and Stacked Charts

--- src/pages/Charts/Stacked.jsx
+++ src/pages/Charts/Stacked.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 
+import { Header, Stacked as StackedChart } from '../../components'
+
 const Stacked = () => {
   return (
-    <div>Stacked</div>
+    <div className="m-4 md:m-10 mt-24  p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl">
+      <Header category='Chart' title='Revenue Breakdown' />
+      <div className='w-full'>
+        <StackedChart />
+      </div>
+    </div>
   )
 }
 
