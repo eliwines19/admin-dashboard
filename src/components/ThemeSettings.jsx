ba7e7581b732670ca1cf270a60eef0039cdaf208
Elijah Wines <wines.elijah@gmail.com>
2024-03-12 20:00:23 -0400

    updated background image and removed dark mode text changes for ecommerce page

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -13,10 +13,6 @@ const ThemeSettings = () => {
 
   const { setColor, setMode, currentMode, currentColor, setThemeSettings } = useStateContext()
 
-  // const handleChange = (e) => {
-  //   setColor(e.value)
-  // }
-
   return (
     <div className='bg-half-transparent w-screen fixed nav-item top-0 right-0'>
       <div className='float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400'>
--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ module.exports = {
         },
         backgroundImage: {
           'hero-pattern':
-            "url('https://i.ibb.co/MkvLDfb/Rectangle-4389.png')",
+            "url('https://i.ibb.co/JnfGrKJ/vecteezy-financial-growth-arrows-with-colorful-vector-illustration-7872285.jpg')",
         },
       },
     },
