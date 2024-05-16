2ca9c1d693a3c42059a5bab8f5aec84e93dc7088
Elijah Wines <wines.elijah@gmail.com>
2024-05-16 18:55:43 -0400

    Bar Chart now shows top 3 selling products

--- src/pages/Charts/Bar.jsx
+++ src/pages/Charts/Bar.jsx
@@ -1,18 +1,68 @@
 import React from 'react';
 import { ChartComponent, SeriesCollectionDirective, SeriesDirective, Inject, Legend, Category, Tooltip, ColumnSeries, DataLabel } from '@syncfusion/ej2-react-charts';
 
-import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
+// import { barCustomSeries, barPrimaryXAxis, barPrimaryYAxis } from '../../data/dummy';
 import { Header } from '../../components';
 import { useStateContext } from '../../contexts/ContextProvider';
 
 const Bar = () => {
 
-  const { currentMode } = useStateContext()
+  const { currentMode, currentYear, sales } = useStateContext()
   const bgColor = currentMode === 'Dark' ? '#33373E' : '#fff'
+
+  const getChartData = () => {
+
+    const productCount = {}
+
+    sales.forEach((sale) => {
+      const { productName } = sale;
+      if(!productCount[productName]){
+        productCount[productName] = 0;
+      }
+      productCount[productName] += 1
+    })
+
+    const sortedProductCount = Object.entries(productCount)
+      .map(([productName, count]) => ({ productName, count }))
+      .sort((a, b) => b.count - a.count);
+
+    const top3Products = sortedProductCount.slice(0, 3)
+
+    return top3Products
+
+  }
+
+  const barCustomSeries = [{
+    dataSource: getChartData(),
+    xName: 'productName',
+    yName: 'count',
+    name: 'Product',
+    type: 'Column',
+    marker: {
+      dataLabel: {
+        visible: true,
+        position: 'Top',
+        font: { fontWeight: '600', color: '#ffffff' },
+      },
+    },
+  }];
+
+  const barPrimaryXAxis = {
+    valueType: 'Category',
+    interval: 1,
+    majorGridLines: { width: 0 },
+  };
+
+  const barPrimaryYAxis = {
+    majorGridLines: { width: 0 },
+    majorTickLines: { width: 0 },
+    lineStyle: { width: 0 },
+    labelStyle: { color: 'transparent' },
+  };
 
   return (
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
-      <Header category="Chart" title="Olympic Medal Counts - RIO" />
+      <Header category="Chart" title={`Top Selling Products (${currentYear})`} />
       <ChartComponent
           id="charts"
           primaryXAxis={barPrimaryXAxis}
